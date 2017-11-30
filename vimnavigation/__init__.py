ff34168de0fa556ecbc85dc4f2a1dff314f4ab0d
Richard Guay <raguay@customct.com>
2017-11-30 14:04:50 -0500

    fixing return calling function

--- vimnavigation/__init__.py
+++ vimnavigation/__init__.py
@@ -1,11 +1,14 @@
-from fman import DirectoryPaneCommand, show_alert
+from fman import DirectoryPaneCommand
 from fman.url import as_human_readable
 from fman.url import as_url
 from os.path import isdir, realpath
-from os import listdir
+
 
 class OpenDirectory(DirectoryPaneCommand):
-	def __call__(self, url=None):
-		file_under_cursor = as_human_readable(self.pane.get_file_under_cursor())
-		if file_under_cursor and isdir(file_under_cursor):
-			self.pane.set_path(as_url(realpath(file_under_cursor)))
+    def __call__(self, url=None):
+        file_under_cursor = as_human_readable(self.pane.get_file_under_cursor())
+        if url is None:
+            if file_under_cursor and isdir(file_under_cursor):
+                self.pane.set_path(as_url(realpath(file_under_cursor)))
+        else:
+            self.pane.set_path(url)
