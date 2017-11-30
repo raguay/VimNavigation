4918711c2207f6035a724cdcddeef94b3e0ef057
Richard Guay <raguay@customct.com>
2017-11-30 12:11:21 -0500

    Updating to new path API

--- vimnavigation/__init__.py
+++ vimnavigation/__init__.py
@@ -1,9 +1,11 @@
 from fman import DirectoryPaneCommand, show_alert
+from fman.url import as_human_readable
+from fman.url import as_url
 from os.path import isdir, realpath
 from os import listdir
 
 class OpenDirectory(DirectoryPaneCommand):
 	def __call__(self):
-		file_under_cursor = self.pane.get_file_under_cursor()
+		file_under_cursor = as_human_readable(self.pane.get_file_under_cursor())
 		if file_under_cursor and isdir(file_under_cursor):
-			self.pane.set_path(realpath(file_under_cursor))
+			self.pane.set_path(as_url(realpath(file_under_cursor)))
